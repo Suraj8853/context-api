5851d324bbb4998d299df950a2c4900687781355
Suraj Pai <surajpai8853@gmail.com>
2023-06-02 04:15:27 -0700

    added routes for routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
 import './App.css';
+import Cart from './Cart';
+import Header from './Header';
+import Home from './Home';
+
 
 function App() {
   return (
-   <div>App</div>
+  <>
+  <BrowserRouter>
+  <Header/>
+<Routes>
+  <Route path='/' element={<Home/>}></Route>
+  <Route path='/cart' element={<Cart />}></Route>
+</Routes>
+  </BrowserRouter>
+  </>
   );
 }
 
