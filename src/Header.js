588b313fa01a8f92315b478fe4dc9b3dbd03f1ca
Suraj Pai <surajpai8853@gmail.com>
2023-06-02 04:46:16 -0700

    Add Link Routing for navigation menu

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+import './App.css';
 const Header = ()=>{
     return (
     <div>
      <h1>React Context-API</h1>
      <ul className="nav">
-        <li>Home</li>
-        <li>Cart</li>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/cart'>Cart</Link></li>
      </ul>
     </div>
     )
