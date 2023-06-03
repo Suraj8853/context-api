dda2ae01ab0ddc2617a36faef8358c2b615253f3
Suraj Pai <surajpai8853@gmail.com>
2023-06-03 01:58:35 -0700

    added app.js code and markup

--- src/App.js
+++ src/App.js
@@ -4,9 +4,20 @@ import './App.css';
 import Cart from './Cart';
 import Header from './Header';
 import Home from './Home';
+import { faker } from '@faker-js/faker';
+import ProductList from './ProductList';
 
 
 function App() {
+ const products = [...Array(25)].map(()=>{
+ return {
+  id:faker.string.uuid(),
+  Name:faker.commerce.product(),
+  Price:faker.commerce.price(),
+  image:faker.image.url()
+ }
+ })
+ console.log(products);
   return (
   <>
   <BrowserRouter>
@@ -15,6 +26,7 @@ function App() {
   <Route path='/' element={<Home/>}></Route>
   <Route path='/cart' element={<Cart />}></Route>
 </Routes>
+<ProductList items={products} />
   </BrowserRouter>
   </>
   );
