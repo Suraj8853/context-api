cffc4ff5007bedfd25c25df9da17e5f06c2d7752
Suraj Pai <surajpai8853@gmail.com>
2023-06-10 02:59:39 -0700

    added Cart ui

--- src/Cart.js
+++ src/Cart.js
@@ -1,6 +1,30 @@
+import { useContext } from "react";
+import { useEffect, useState } from "react";
+import CartContext from "./context/context";
+import Product from "./Product";
+
+
 const Cart = ()=>{
+    const [total,setTotal]=useState();
+    const {cart} =useContext(CartContext);
+
+    useEffect(()=>{
+    setTotal(cart.reduce((acc,cur)=>{
+    return acc+parseInt(cur.Price)
+    },0));
+    },[cart])
+    debugger;
+    const productItems = cart.map((item)=>{
+        return <Product product={item} cart={cart} />
+       })
     return (
-        <div>Cart</div>
+        <div>
+            <span style={{fontSize:'33px'}}>My Cart</span>
+            <span style={{fontSize:'33px'}}>Total :{total}</span>
+             <div className="product-list">
+            {productItems}
+             </div>
+        </div>
     )
 }
 export default Cart;
