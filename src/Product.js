6667ff23ec079b81b94fddac33b7f186e16d58ab
Suraj Pai <surajpai8853@gmail.com>
2023-06-10 03:04:56 -0700

    added product

--- src/Product.js
+++ src/Product.js
@@ -1,13 +1,28 @@
-const Product = ({product})=>{
+import { useContext } from "react";
+import CartContext from "./context/context";
+
+
+const Product = ({product,cart})=>{
+  debugger;
+  const {addCarts,removeFromCart}=useContext(CartContext)
+  let content='';
+
+   if(cart.includes(product)) {
+   content=<button onClick={()=>removeFromCart(product)}>Remove From Cart</button>
+  }
+ 
+   else{
+  content= <button onClick={()=>addCarts(product)}>Add To Cart</button>
+  }            
     return (
-        <div className="product-name">
+        <div key={product.id} className="product-name">
           <img src={product.image} alt="desc" />
           <div className="product-content">
             <div className="product-details">
             <label>{product.Name}</label>
             <label>Price : {product.Price}</label>
             </div>
-            <button>Add To Cart</button>
+             {content}     
           </div>
         </div>
     )
