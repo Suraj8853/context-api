599cd9fb6f58f9e43ae152565fd2dc58cdacbb4e
Suraj Pai <surajpai8853@gmail.com>
2023-06-10 02:59:22 -0700

    added App.js code

--- src/App.js
+++ src/App.js
@@ -3,30 +3,33 @@ import { BrowserRouter,Route,Routes } from 'react-router-dom';
 import './App.css';
 import Cart from './Cart';
 import Header from './Header';
-import Home from './Home';
 import { faker } from '@faker-js/faker';
 import ProductList from './ProductList';
+import { useState } from 'react';
+import { Provider } from './context/context';
 
 
 function App() {
- const products = [...Array(25)].map(()=>{
- return {
-  id:faker.string.uuid(),
-  Name:faker.commerce.product(),
-  Price:faker.commerce.price(),
-  image:faker.image.url()
- }
- })
- console.log(products);
+  const products = [...Array(25)].map(()=>{
+    return {
+     id:faker.string.uuid(),
+     Name:faker.commerce.product(),
+     Price:faker.commerce.price(),
+     image:faker.image.urlLoremFlickr()
+    }
+    })
+    const [productArr]=useState(products)
+ 
   return (
   <>
   <BrowserRouter>
   <Header/>
+  <Provider>
 <Routes>
-  <Route path='/' element={<Home/>}></Route>
-  <Route path='/cart' element={<Cart />}></Route>
+  <Route path='/' element={<ProductList  items={productArr}/>}></Route>
+  <Route path='/cart' element={<Cart  />}></Route>
 </Routes>
-<ProductList items={products} />
+</Provider>
   </BrowserRouter>
   </>
   );
