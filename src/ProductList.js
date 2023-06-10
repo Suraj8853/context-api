a146cdb5e1538c91e3d9ccd7d9bd3a85430d0e27
Suraj Pai <surajpai8853@gmail.com>
2023-06-10 03:05:19 -0700

    added logic and markup for product-ui

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,8 +1,12 @@
+import { useContext } from "react";
+import CartContext from "./context/context";
 import Product from "./Product";
 
 const ProductList = ({items})=>{
+    debugger;
+    const {addCarts,removeFromCart,cart}=useContext(CartContext);
     const productItems = items.map((item)=>{
-     return <Product product={item} />
+     return <Product cart={cart} addCarts={addCarts} removeFromCart={removeFromCart}  product={item} />
     })
     return (
         <div className="product-list">
